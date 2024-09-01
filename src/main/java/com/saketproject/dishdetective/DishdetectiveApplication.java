b4c2d893da43194cf3dc6b2a2159bfdcb5622a1c
Saket Gandham <stgandham@gmail.com>
2024-09-01 17:37:19 -0400

    first commit

---
+++ src/main/java/com/saketproject/dishdetective/DishdetectiveApplication.java
@@ -0,0 +1,13 @@
+package com.saketproject.dishdetective;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishdetectiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishdetectiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saketproject/dishdetective/DishdetectiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saketproject.dishdetective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishdetectiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
